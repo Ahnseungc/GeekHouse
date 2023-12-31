70a39443395e428ce93e21b43b444f6ecd53c931
Ahnseungc <94547692+Ahnseungc@users.noreply.github.com>
2024-01-01 04:32:22 +0900

    feat: 테마 스타일 & 타입 정의

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  styledComponents: true,
+  compiler: {
+    styledComponents: true,
+  },
 };
 
 module.exports = nextConfig;
---
+++ types/db.ts
@@ -0,0 +1,11 @@
+export interface ChildProps {
+  children: JSX.Element;
+}
+
+export interface ThemeButtonProps {
+  currentTheme: number;
+}
+
+export interface MonacoEditorProps {
+  currentTheme: number;
+}
